2b5121e2892140b305fa17703a437ff1ecadcdce
keerthana <keerthana.a2019@gmail.com>
2024-07-30 18:48:48 +0530

    angular

---
+++ Angular/MyAngularProject1/src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  name :string;
+  age : number;
+  email: string;
+  imagePath: string;
+
+constructor(){
+  this.name="keerthana";
+  this.age=20;
+  this.email="priya@gmail.com";
+  this.imagePath="https://miro.medium.com/v2/resize:fit:720/format:webp/1*lqMtWmTLHxT_6SN4Kjvaog.png"
+}
+chaneName(){
+  this.name="Mouni";
+}
+changeAge(){
+  this.age=21;
+}
+changeEmail(){
+  this.email="mouni@gmail.com";
+}
+changeImage(){
+  this.imagePath="https://miro.medium.com/v2/resize:fit:720/format:webp/1*bMgQ8MhbnQexpqHgIgBJPA.png"
+}
+
+
+  // title = 'MyAngularProject1';
+  // flag:boolean;
+  // vehicles:string[];
+  // selectedVehicle :string;
+  // myStyle: {};
+  // myClass: string;
+  // constructor(){
+  //   this.flag=true;
+  //   this.vehicles=["Twowheeler","Threewheeler","Fourwheeler"];
+  //   this.selectedVehicle="";
+  //   this.myStyle={'width': '40%', 'border':'2px solid green','border-radius':'25px'};
+  //   this.myClass="MyClass1";
+  // }
+  // changeFlag(){
+  //   this.flag= ! this.flag;
+  // }
+  // setSelectedItem(vec: string){
+  //     this.selectedVehicle=vec;
+  // }
+  // changeStyle(){
+  //   this.myStyle= {'width': '40%', 'border':'2px solid red','border-radius':'25px'};
+  // }
+}
